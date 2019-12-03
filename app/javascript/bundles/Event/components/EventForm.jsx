6157e2d45c7d997c3ed87035dbb6e54dc4144550
Emilio Izquierdo <emilio@Emilios-MacBook-Pro.local>
2019-12-03 16:04:43 -0500

    ...

--- app/javascript/bundles/Event/components/EventForm.jsx
+++ app/javascript/bundles/Event/components/EventForm.jsx
@@ -1,78 +1,69 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 
-export const headers = ReactOnRails.authenticityHeaders()
+export const headers = ReactOnRails.authenticityHeaders();
+
+const eventTypeOptions = [
+  { label: "Select Type", value: "" },
+  { label: "Prescription", value: "prescriptions" },
+  { label: "Contact", value: "contacts" }
+];
 
 export default class EventForm extends React.Component {
-  state = { 
-    eventTypeOptions: {
-                        'Select Type':    '',
-                        'Prescription':   'prescriptions',
-                        'Contact':        'contacts'
-                      },
+  state = {
     eventTypeData: [],
-    eventValues: { type: '',name: '',date: '',time: '',notes: '' }
-  }
+    eventValues: { type: "", name: "", date: "", time: "", notes: "" }
+  };
 
-  fetchData = (eventTypeValue) => {
-    axios.get(`/${eventTypeValue}.json`)
-    .then((response => {
-      const data = response.data
-      this.setState({ eventTypeData: data})
-      }
-    ))
-  }
-
-  optionsArray = (s) => {
-    let keys = []
-    let values = []
-    for (const property in s) {
-      keys.push(property)
-      values.push(s[property])
-    }
-     let options = keys.map((key,i) => {
-        return(
-          <option key={key} value={values[i]}>{key}</option>
-        )
-      })
-      return options
-    }
-
-    handleInputChange = field => e => {
-      let value = e.target.value.trim()
-      if (value === 'contacts') {
-          this.fetchData(value)
-          value = 'Contact'
-        } else if (value === 'prescriptions') {
-          this.fetchData(value)
-          value = 'Prescription'
-        }
-      this.setState(prevState => ({
-        eventValues: {
-          ...prevState.eventValues,
-          [field]: value
-          }
-        }))
-    }
-
-    handleSubmit = async e => {
+  fetchData = async e => {
     const { eventValues } = this.state;
-    const { data } =  await axios.post("events#create", { ...eventValues }, { headers })
-    }
+    const type = e.target.value;
+    const normalizedType =
+      type[0].toUpperCase() + type.substr(1, type.length - 2);
+    const { data } = await axios.get(`/${type}.json`);
+    this.setState({
+      eventTypeData: data,
+      eventValues: { ...eventValues, name: data[0].id, type: normalizedType }
+    });
+  };
+
+  handleInputChange = field => e => {
+    const value = e.target.value.trim();
+    this.setState(prevState => ({
+      eventValues: { ...prevState.eventValues, [field]: value }
+    }));
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    const { eventValues } = this.state;
+    await axios.post("/events", eventValues, { headers });
+    Turbolinks.visit("/events");
+  };
+
   
   render() {
+    const { eventTypeData } = this.state;
     return(
       <form id='add-event' onSubmit={this.handleSubmit}>
         <div id='add-event-status-bar'></div>
         <div id='add-event-header'>New Activity</div>
         <div id='add-event-form-fields'>
           <div className='add-event-form-field-label'>Type</div>
-              <select onChange={this.handleInputChange('type')} >
-                {this.optionsArray(this.state.eventTypeOptions).map(option => option)}
+              <select onChange={this.fetchData} >
+              {eventTypeOptions.map(({ label, value }) => (
+                <option key={value} value={value}>
+                  {label}
+                </option>
+              ))}
               </select>
           <div className='add-event-form-field-label'>Name</div>
               <select onChange={this.handleInputChange('name')} >
-                {this.state.eventTypeData.map(data => <option key={data.id} value={data.id}>{data.name}</option> )}
+              {eventTypeData.map(data => (
+                <option key={data.id} value={data.id}>
+                  {data.name}
+                </option>
+              ))}
               </select>
           <div className='add-event-form-field-label'>Date</div>
             <input id="date" onChange={this.handleInputChange('date')} type="date" />
