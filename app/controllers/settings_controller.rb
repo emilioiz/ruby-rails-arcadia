e4b60c8bc164ab60a89f1b48bf94c12d77e16369
Emilio Izquierdo <emilio@Emilios-MacBook-Pro.local>
2019-12-03 19:29:31 -0500

    ...

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,6 +1,14 @@
 class SettingsController < ApplicationController
 
   def index
+    @settings = [
+      { label: 'App Design', icon_url: 'https://arcadia-static-assets.s3.amazonaws.com/arcadia-svgs/SettingsNextButton.svg' },
+      { label: 'Change Language', icon_url: 'https://arcadia-static-assets.s3.amazonaws.com/arcadia-svgs/SettingsNextButton.svg'  },
+      { label: 'Enable FitBit', icon_url: 'https://arcadia-static-assets.s3.amazonaws.com/arcadia-svgs/SettingsNextButton.svg'  },
+      { label: 'Notifications', icon_url: 'https://arcadia-static-assets.s3.amazonaws.com/arcadia-svgs/SettingsNextButton.svg'  },
+      { label: 'Metric System', icon_url: 'https://arcadia-static-assets.s3.amazonaws.com/arcadia-svgs/SettingsNextButton.svg'  },
+      { label: 'Health Insights', icon_url: 'https://arcadia-static-assets.s3.amazonaws.com/arcadia-svgs/SettingsNextButton.svg'  },
+  ]
   end
 
 end
